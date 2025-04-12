2fe1cc246af38869902172796efb57922a6b32c1
Olena Surilova <elen21surilova@gmail.com>
2025-04-12 23:10:22 +0200

    refactor, hw-12

--- homework-lecture-6/12/app.js
+++ homework-lecture-6/12/app.js
@@ -1,11 +1,12 @@
 // Зробити REST API на Express для керування завданнями, де всі дані зберігаються у файлі tasks.json. API має повністю працювати з цим файлом: при кожній зміні — перезапис, при кожному читанні — зчитування з файлу.
 const path = require('path');
-const { readFile, writeFile } = require('fs/promises');
 
-const {  v4: uuidv4 } = require('uuid');
 const methodOverride = require('method-override');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const tasksRouter = require('./routes/taskRoutes');
+const readFileJson = require('./utils/readFileJson');
 
 const app = express();
 const port = 3000;
@@ -23,74 +24,7 @@ app.use(methodOverride(function (req, res) {
 
 const pathFile = path.join(__dirname, 'data', 'tasks.json');
 
-async function readFileJson(path) {
-    const content = await readFile(path, { encoding: 'utf-8'});
-    return JSON.parse(content);
-}
-
-async function writeFileJson(path, tasks) {
-  try {
-    await writeFile(path, JSON.stringify(tasks, null, 2));
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-app.get('/tasks/:id', async (req, res, next) => {
-  const id = req.params.id;
-  const tasks = await readFileJson(pathFile);
-  const task = tasks.find(item => item.id === id);
-  if (!task) {
-    return res.status(404).send('Task not found');
-  }
-  res.render('task', { 'task': task });
-});
-
-app.put('/tasks/:id', async (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  const tasks = await readFileJson(pathFile);
-  const task = tasks.find(item => item.id === id);
-  if (!task) {
-    return res.status(404).send('Task not found');
-  }
-
-  task.title = task.title !== req.body.title ? req.body.title : task.title;
-  task.description = task.description !== req.body.description ? req.body.description : task.description;
-  task.status = task.status !== req.body.status ? req.body.status : task.status;
-  task.modifiedAt = new Date();
-
-  await writeFileJson(pathFile, tasks);
-  res.redirect('/');
-});
-
-app.delete('/tasks/:id', async (req, res, next) => {
-  const id = req.params.id;
-  const tasks = await readFileJson(pathFile);
-  const newTasks = tasks.filter(item => item.id !== id);
-
-  await writeFileJson(pathFile, newTasks);
-  res.redirect('/');
-});
-
-app.post('/tasks', async (req, res, next) => {
-  const tasks = await readFileJson(pathFile);
-  const taskObj = {
-    id: uuidv4(),
-    title: req.body.title,
-    description: req.body.description,
-    status: req.body.status,
-    createdAt: new Date()
-  };
-  tasks.push(taskObj);
-  
-  await writeFileJson(pathFile, tasks);
-  res.redirect('/');
-});
-
-app.get('/tasks', (req, res, next) => {
-  res.redirect('/');
-});
+app.use(tasksRouter);
 
 app.use('/', async(req, res, next) => {
   try {
--- homework-lecture-6/12/routes/taskRoutes.js
+++ homework-lecture-6/12/routes/taskRoutes.js
@@ -4,3 +4,73 @@ GET /tasks/:id — отримати одне завдання
 POST /tasks — додати нове
 PUT /tasks/:id — оновити існуюче
 DELETE /tasks/:id — видалити */
+const path = require('path');
+
+const express = require('express');
+const {  v4: uuidv4 } = require('uuid'); 
+
+const writeFileJson = require('../utils/writeFileJson');
+const readFileJson = require('../utils/readFileJson');
+
+const router = express.Router();
+
+const pathFile = path.join(__dirname, '..', 'data', 'tasks.json');
+
+router.get('/tasks/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const tasks = await readFileJson(pathFile);
+  const task = tasks.find(item => item.id === id);
+  if (!task) {
+    return res.status(404).send('Task not found');
+  }
+  res.render('task', { 'task': task });
+});
+
+router.put('/tasks/:id', async (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
+  const tasks = await readFileJson(pathFile);
+  const task = tasks.find(item => item.id === id);
+  if (!task) {
+    return res.status(404).send('Task not found');
+  }
+
+  task.title = task.title !== req.body.title ? req.body.title : task.title;
+  task.description = task.description !== req.body.description ? req.body.description : task.description;
+  task.status = task.status !== req.body.status ? req.body.status : task.status;
+  task.modifiedAt = new Date();
+
+  await writeFileJson(pathFile, tasks);
+  res.redirect('/');
+});
+
+router.delete('/tasks/:id', async (req, res, next) => {
+  const id = req.params.id;
+  const tasks = await readFileJson(pathFile);
+  const newTasks = tasks.filter(item => item.id !== id);
+
+  await writeFileJson(pathFile, newTasks);
+  res.redirect('/');
+});
+
+router.post('/tasks', async (req, res, next) => {
+  const tasks = await readFileJson(pathFile);
+  const taskObj = {
+    id: uuidv4(),
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    createdAt: new Date()
+  };
+  tasks.push(taskObj);
+  
+  await writeFileJson(pathFile, tasks);
+  res.redirect('/');
+});
+
+router.get('/tasks', (req, res, next) => {
+  res.redirect('/');
+});
+
+
+module.exports = router;
