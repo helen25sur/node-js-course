6203c807429767d8f0d3ac1969f34ee736fdb03c
Olena Surilova <elen21surilova@gmail.com>
2025-05-24 14:22:13 +0200

    feat: add order's route

--- homework-lecture-11/26/backend/routes/orderRoutes.js
+++ homework-lecture-11/26/backend/routes/orderRoutes.js
@@ -6,8 +6,25 @@ const router = express.Router();
 
 const ObjectId = mongodb.ObjectId;
 
-router.get('/orders', function (req, res) {
-  
+router.get('/orders', async function (req, res) {
+  try {
+    const database = await db.getDB();
+    const orders = await database
+      .collection('orders')
+      .find()
+      .toArray();
+    console.log(orders[0].items);
+    const products = await database
+      .collection('products')
+      .find()
+      .toArray();
+    console.log(products)
+    res.render('orders-list', { orders, products, title: 'Orders List' });
+  } catch (error) {
+    console.error('Error fetching orders:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+
+  }
 });
 
 module.exports = router;
